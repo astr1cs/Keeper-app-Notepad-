98db2b087da968378c3e1d5568ac8fdaadab4b4f
MD MERAZ UDDIN <merazuddin003@gmail.com>
2024-08-10 04:33:25 +0600

    Updates

---
+++ precache-manifest.cd353a235aede1fec3f59040e0465979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa3745dc92d9ef316e54695a08dee10",
+    "url": "/Keeper-app-Notepad-/index.html"
+  },
+  {
+    "revision": "ada4300f8789c5b0fd3f",
+    "url": "/Keeper-app-Notepad-/static/js/2.1d59e991.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Keeper-app-Notepad-/static/js/2.1d59e991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7656827a8102b7c5bb",
+    "url": "/Keeper-app-Notepad-/static/js/main.0cd9ed87.chunk.js"
+  },
+  {
+    "revision": "362d6879223c2328aad0",
+    "url": "/Keeper-app-Notepad-/static/js/runtime-main.799b7703.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0127363c9ec894140864771dde595595.js"
+  "/Keeper-app-Notepad-/precache-manifest.cd353a235aede1fec3f59040e0465979.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Keeper-app-Notepad-/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
