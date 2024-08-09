eed441d0a078825edb457864269f30437166d18e
MD MERAZ UDDIN <merazuddin003@gmail.com>
2024-08-10 04:32:07 +0600

    Updates

---
+++ precache-manifest.0127363c9ec894140864771dde595595.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141397cc13a0112bafa72eeddf6b7693",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada4300f8789c5b0fd3f",
+    "url": "/static/js/2.1d59e991.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.1d59e991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7656827a8102b7c5bb",
+    "url": "/static/js/main.0cd9ed87.chunk.js"
+  },
+  {
+    "revision": "92cf6c8e8d20f5f3d0ee",
+    "url": "/static/js/runtime-main.86448286.js"
+  }
+]);
